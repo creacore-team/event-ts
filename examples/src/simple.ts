3f8a6fa7a3fb056ce68fc561ccc19a6b56e062de
nairod <dorian.baguette@gmail.com>
2018-06-04 23:50:04 +0200

    update first example : simple

--- examples/src/simple.ts
+++ examples/src/simple.ts
@@ -1,21 +1,46 @@
 import {Event, EventManager} from "./../../dist/index"
+// local import
 
+//Creation of a new Event with no parameter
 @Event({})
 class FirstSimpleEvent
 {
 
 }
 
+//Creation of a new Event with one readonly parameter
 @Event({})
 class SecondSimpleEvent
 {
     constructor(public readonly value:number){}
 }
 
-EventManager.addEventListener(FirstSimpleEvent,(ev)=>{console.log("FirstSimpleEvent catched ! queued :", ev.queued, "- following :", ev.following)});
-EventManager.addEventListener(SecondSimpleEvent,(ev)=>{console.log("SecondSimpleEvent catched ! value :", ev.value)});
+//Creation of an artificial emitter
+let myEmitter = {}
 
+// Adding a listener for FirstSimpleEvent with no specific emitter, all FirstSimpleEvent are catched
+EventManager.addEventListener(FirstSimpleEvent,(ev) => {
+    console.log("FirstSimpleEvent with NO specific emitter catched ! is it myEmitter : ", myEmitter == ev.emitter )
+});
+
+// Adding a listener for FirstSimpleEvent when the emitter is myEmitter
+EventManager.addEventListener(FirstSimpleEvent,(ev) => {
+    console.log("FirstSimpleEvent with specific emitter catched ! is it myEmitter : ", myEmitter == ev.emitter )
+},myEmitter);
+
+// Adding a listener for SecondSimpleEvent with no specific emitter, all SecondSimpleEvent are catched
+EventManager.addEventListener(SecondSimpleEvent,(ev) => {
+    console.log("SecondSimpleEvent catched ! value :", ev.value)
+});
+
+// Dispatching a FirstSimpleEvent with no specific emitter
 EventManager.dispatchEvent(new FirstSimpleEvent());
 
+// Dispatching a FirstSimpleEvent with myEmitter as emitter
+EventManager.dispatchEvent(new FirstSimpleEvent(),myEmitter);
+
+// Dispatching a SecondSimpleEvent with value = 5 and no emitter
 EventManager.dispatchEvent(new SecondSimpleEvent(5));
+
+// Dispatching a SecondSimpleEvent with value = 10 and no emitter
 EventManager.dispatchEvent(new SecondSimpleEvent(10));
