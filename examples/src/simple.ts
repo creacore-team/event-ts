094edc164929aa8743f02f9f525935007302ab93
nairod <dorian.baguette@gmail.com>
2018-06-04 23:36:12 +0200

    adding and configuring examples

---
+++ examples/src/simple.ts
@@ -0,0 +1,21 @@
+import {Event, EventManager} from "./../../dist/index"
+
+@Event({})
+class FirstSimpleEvent
+{
+
+}
+
+@Event({})
+class SecondSimpleEvent
+{
+    constructor(public readonly value:number){}
+}
+
+EventManager.addEventListener(FirstSimpleEvent,(ev)=>{console.log("FirstSimpleEvent catched ! queued :", ev.queued, "- following :", ev.following)});
+EventManager.addEventListener(SecondSimpleEvent,(ev)=>{console.log("SecondSimpleEvent catched ! value :", ev.value)});
+
+EventManager.dispatchEvent(new FirstSimpleEvent(),{});
+
+EventManager.dispatchEvent(new SecondSimpleEvent(5),{});
+EventManager.dispatchEvent(new SecondSimpleEvent(10),{});
